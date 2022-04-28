b05f4f44db66a020b963d4ea7b1560111df8d8c3
sinapradip <101270811+sinapradip@users.noreply.github.com>
2022-04-28 13:06:55 +0545

    Removed loop and added query selector for each

--- index.js
+++ index.js
@@ -1,24 +1,52 @@
+// Query selector for each of letters beacused looping is slow in live site.
 
-for(let i=0;i<document.querySelectorAll(".drum").length;i++)
-{
-    document.querySelectorAll("button")[i].addEventListener("click", 
-    
-    function ()
-    {
-        var audio = new Audio("sounds/"+i+".mp3");
-        audio.play();
-        
-        
-
-    }
-    );
+document.querySelectorAll("button")[0].addEventListener( "click", function () {
+    var audio = new Audio("sounds/0.mp3");
+    audio.play();
 }
+);
 
-document.addEventListener("keypress", 
-    function (event) {
-        if (event == "w")
-        {
-            var audio = new Audio("sounds/0.mp3");
-            audio.play();
-        }
-    });
+
+document.querySelectorAll("button")[1].addEventListener( "click", function () {
+    var audio = new Audio("sounds/1.mp3");
+    audio.play();
+}
+);
+
+document.querySelectorAll("button")[2].addEventListener( "click", function () {
+    var audio = new Audio("sounds/2.mp3");
+    audio.play();
+}
+);
+
+document.querySelectorAll("button")[2].addEventListener( "click", function () {
+    var audio = new Audio("sounds/2.mp3");
+    audio.play();
+}
+);
+
+document.querySelectorAll("button")[3].addEventListener( "click", function () {
+    var audio = new Audio("sounds/3.mp3");
+    audio.play();
+}
+);
+
+document.querySelectorAll("button")[4].addEventListener( "click", function () {
+    var audio = new Audio("sounds/4.mp3");
+    audio.play();
+}
+);
+
+document.querySelectorAll("button")[5].addEventListener( "click", function () {
+    var audio = new Audio("sounds/5.mp3");
+    audio.play();
+}
+);
+
+document.querySelectorAll("button")[6].addEventListener( "click", function () {
+    var audio = new Audio("sounds/6.mp3");
+    audio.play();
+}
+);
+
+// End of query selector for each click
